c4e107bebe6c5d4715259297d59aa9be047394fa
bintoromaheso-boop <bintoromaheso@gmail.com>
2026-05-23 15:57:04 +0700

    Update register.js

--- login/js/register.js
+++ login/js/register.js
@@ -1,7 +1,7 @@
 document.getElementById("registerForm").addEventListener("submit", async function(e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
+    const username = document.getElementById("firstName").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
